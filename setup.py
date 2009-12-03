43f272daac05ef539fbdb1e22e863d881e52e565
John Evans <john@milo.com>
2009-12-02 22:19:54 -0800

    Hopefully got a valid fully working installation now.

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from distutils.core import setup
 from distutils.core import Extension
 
 setup(name='jenkins',
-      version='1.0.1',
+      version='1.0.2',
       description="Python ctypes wrapper around Bob Jenkins' hash functions.",
       author="John Evans",
       author_email="john@milo.com",
       url="http://github.com/lgastako/jenkins/",
       py_modules=['jenkins'],
-      ext_modules=[Extension('lookup3', ['src/lookup3.c'])],
+      ext_modules=[Extension('lookup3', ['lookup3.c'])],
       )
