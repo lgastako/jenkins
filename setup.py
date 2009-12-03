ae7cbc63327b0f4e434a910818ee82e5a17756f7
John Evans <john@milo.com>
2009-12-02 21:07:13 -0800

    Moved building of C shared object into setup.py, nuked Makefile.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+from distutils.core import Extension
+
+setup(name='jenkins',
+      version='1.0',
+      description="Python ctypes wrapper around Bob Jenkins' hash functions.",
+      author="John Evans",
+      author_email="john@milo.com",
+#      url="
+      py_modules=['jenkins'],
+      ext_modules=[Extension('lookup3', ['lookup3.c'])],
+      )
